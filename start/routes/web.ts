0bcbbd46b669f1ad97f2c1afffa5182a178b5dd0
MMR <moj2raj2@gmail.com>
2025-04-21 01:40:04 +0330

    init bajak

--- start/routes/web.ts
+++ start/routes/web.ts
@@ -22,15 +22,16 @@ import collect from 'collect.js'
 import UserFinancial from '#models/user_financial'
 export default function () {
   router.get('test', async () => {
+    return await db.rawQuery(`
+      ALTER TABLE user_financials
+      ALTER COLUMN balance TYPE INTEGER USING balance::INTEGER,
+ALTER COLUMN balance SET DEFAULT 0;
+    `)
+
     await Transaction.query().where('type', 'winwheel').delete()
     await UserFinancial.query().update({ balance: 1000000 })
     return
-    return await db.rawQuery(`
-     ALTER TABLE daberna
-ALTER COLUMN boards
-TYPE jsonb
-USING boards::jsonb;
-    `)
+
     return
     return await Setting.create({
       key: 'blackjack_help',
