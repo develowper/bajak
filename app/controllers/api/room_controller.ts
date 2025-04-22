90bf16d573c5d19c67c6d79804384e7ef77652fb
MMR <moj2raj2@gmail.com>
2025-04-22 21:52:16 +0330

    init bajak

--- app/controllers/api/room_controller.ts
+++ app/controllers/api/room_controller.ts
@@ -231,7 +231,7 @@ export default class RoomController {
       if (beforeIpExists) {
         await trx.rollback()
         return response.status(400).json({
-          message: i18n.t('messages.validate.duplicate_*', { value: 'ip' }),
+          message: i18n.t('messages.validate.duplicate_*', { item: 'ip' }),
         })
       }
       const userFinancials = await UserFinancial.firstOrCreate(
