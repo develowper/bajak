10519d3dd353d996eb41e3e7ece7288dff0a4e76
MMR <moj2raj2@gmail.com>
2025-04-24 15:52:51 +0330

    init bajak

--- inertia/pages/Panel/Admin/Transaction/Index.vue
+++ inertia/pages/Panel/Admin/Transaction/Index.vue
@@ -130,7 +130,7 @@
                   type="button"
                   @click="(params.payed_at = params.payed_at === 1 ? null : 1), getData()"
                   class="inline-block select-none border-2 w-24 p-2 text-center text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out hover:border-primary-accent-200 focus:border-primary-accent-200 focus:bg-secondary-50/50 focus:outline-none focus:ring-0 active:border-primary-accent-200 motion-reduce:transition-none dark:border-primary-400 dark:text-primary-300 dark:hover:bg-blue-950 dark:focus:bg-blue-950"
-                  :class="`  cursor-pointer ${'rounded-s-lg'} border-dark-500 ${1 === params.payed_at ? `text-white bg-green-500` : `text-gray-500 bg-white`}`"
+                  :class="`  cursor-pointer ${'rounded-s-lg'} border-dark-500 ${1 === params.payed_at ? `text-white dark:text-white bg-green-500` : `text-gray-500 bg-white`}`"
                   data-twe-ripple-init
                   data-twe-ripple-color="light"
                 >
@@ -140,7 +140,7 @@
                   type="button"
                   @click="(params.payed_at = params.payed_at === 0 ? null : 0), getData()"
                   class="inline-block select-none border-2 w-24 p-2 text-center text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out hover:border-primary-accent-200 focus:border-primary-accent-200 focus:bg-secondary-50/50 focus:outline-none focus:ring-0 active:border-primary-accent-200 motion-reduce:transition-none dark:border-primary-400 dark:text-primary-300 dark:hover:bg-blue-950 dark:focus:bg-blue-950"
-                  :class="`  cursor-pointer ${'rounded-e-lg'} border-dark-500 ${0 === params.payed_at ? `text-white bg-red-500` : `text-gray-500 bg-white`}`"
+                  :class="`  cursor-pointer ${'rounded-e-lg'} border-dark-500 ${0 === params.payed_at ? `text-white dark:text-white bg-red-500` : `text-gray-500 bg-white`}`"
                   data-twe-ripple-init
                   data-twe-ripple-color="light"
                 >
@@ -195,7 +195,7 @@
                       getData('clear')
                   "
                   class="inline-block select-none border-2 w-24 p-2 text-center text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out hover:border-primary-accent-200 focus:border-primary-accent-200 focus:bg-secondary-50/50 focus:outline-none focus:ring-0 active:border-primary-accent-200 motion-reduce:transition-none dark:border-primary-400 dark:text-primary-300 dark:hover:bg-blue-950 dark:focus:bg-blue-950"
-                  :class="`  cursor-pointer ${idx == 0 ? 'rounded-s-lg' : idx == $page.props.types.length - 1 ? 'rounded-e-lg' : ''} border-dark-500 ${s.name === params.type ? `text-white bg-${s.color}-500` : `text-gray-500 bg-white`}`"
+                  :class="`  cursor-pointer ${idx == 0 ? 'rounded-s-lg' : idx == $page.props.types.length - 1 ? 'rounded-e-lg' : ''} border-dark-500 ${s.name === params.type ? `text-white dark:text-white bg-${s.color}-500` : `text-gray-500 bg-white`}`"
                   data-twe-ripple-init
                   data-twe-ripple-color="light"
                 >
