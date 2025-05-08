583b0753fccbfdd5ef36157d60a5ab2251285179
MMR <moj2raj2@gmail.com>
2025-05-08 08:59:00 +0330

    init winner

--- commands/daily_report.ts
+++ commands/daily_report.ts
@@ -21,10 +21,10 @@ export default class DailyReport extends BaseCommand {
   static reportTime = DateTime.fromObject({ hour: 2, minute: 0 }, { zone: 'Asia/Tehran' })
   async run() {
     const now = DateTime.now().setZone('Asia/Tehran')
-    if (now.hour !== DailyReport.reportTime.hour || now.minute !== DailyReport.reportTime.minute) {
-      process.exit()
-      return
-    }
+    // if (now.hour !== DailyReport.reportTime.hour || now.minute !== DailyReport.reportTime.minute) {
+    //   process.exit()
+    //   return
+    // }
 
     const clearPeriodDay = (await getSettings('clear_period_day')) ?? 0
 
