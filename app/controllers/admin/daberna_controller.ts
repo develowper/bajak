cb2e8521a5474153364cb639f28553c6a07f121a
MMR <moj2raj2@gmail.com>
2025-07-21 19:22:57 +0330

    init winner

--- app/controllers/admin/daberna_controller.ts
+++ app/controllers/admin/daberna_controller.ts
@@ -17,7 +17,9 @@ export default class DabernaController {
     sort = ['row_win_prize', 'win_prize', 'card_count'].includes(sort) ? 'id' : sort
     let query = Daberna.query()
     const isPg = isPG()
-    console.log(await Daberna.query().whereRaw(`boards @> '[{"user_id": "${userId}"}]'`))
+    console.log(
+      await Daberna.query().whereRaw(`boards @> '[{"user_id": "${userId}"}]'`).count('* as total')
+    )
     if (userId) {
       if (isPg) query.whereRaw(`boards @> '[{"user_id": "${userId}"}]'`)
       else query.where('boards', 'like', `%id":${userId},%`)
